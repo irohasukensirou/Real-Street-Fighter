ff62d19d99f197f6edd51df1707b363b8c2b6eb2
irohasukensirou <52659785+irohasukensirou@users.noreply.github.com>
2022-02-18 08:20:00 +0900

    Update body.py

--- openpose/body.py
+++ openpose/body.py
@@ -21,7 +21,6 @@ class Body(object):
         self.model.eval()
 
     def __call__(self, oriImg):
-        # scale_search = [0.5, 1.0, 1.5, 2.0]
         scale_search = [0.5]
         boxsize = 368
         stride = 8
